14bed18129294e9d649281e19c340393aacb4819
Michael Phillips <michaeljoelphillips@gmail.com>
2019-10-08 23:34:16 -0500

    Fix bugs introduced in the second branch.

--- app/Account.php
+++ app/Account.php
@@ -4,17 +4,32 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Exceptions\InsufficientFundsException;
+use InvalidArgumentException;
+
 class Account
 {
     private $balance = 0.0;
 
     public function deposit(float $amount): void
     {
+        if ($amount <= 0) {
+            throw new InvalidArgumentException(sprintf('Deposit amount must be greater than zero.'));
+        }
+
         $this->balance += $amount;
     }
 
     public function withdraw(float $amount): float
     {
+        if ($amount <= 0) {
+            throw new InvalidArgumentException(sprintf('Withdrawal amount must be greater than zero.'));
+        }
+
+        if ($this->getBalance() < $amount) {
+            throw new InsufficientFundsException();
+        }
+
         $this->balance -= $amount;
 
         return $amount;
