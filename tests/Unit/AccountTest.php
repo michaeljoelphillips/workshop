81b2f633a59ecd31a4cca44469ec948c088fa0b4
Michael Phillips <michaeljoelphillips@gmail.com>
2019-10-08 23:34:16 -0500

    Fix bugs introduced in the second branch.

--- tests/Unit/AccountTest.php
+++ tests/Unit/AccountTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Tests\Unit;
 
 use App\Account;
+use App\Exceptions\InsufficientFundsException;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 class AccountTest extends TestCase
@@ -28,5 +30,34 @@ class AccountTest extends TestCase
 
         self::assertEquals(200.00, $subject->getBalance());
         self::assertEquals(100.00, $withdrawnFunds);
+    }
+
+    public function testWithdrawWithInsufficientFunds(): void
+    {
+        $subject = new Account();
+
+        self::expectException(InsufficientFundsException::class);
+
+        $subject->withdraw(400.00);
+    }
+
+    public function testWithdrawWithZeroDollars(): void
+    {
+        $subject = new Account();
+
+        self::expectException(InvalidArgumentException::class);
+        self::expectExceptionMessage('Withdrawal amount must be greater than zero.');
+
+        $subject->withdraw(0.00);
+    }
+
+    public function testDepositWithZeroDollars(): void
+    {
+        $subject = new Account();
+
+        self::expectException(InvalidArgumentException::class);
+        self::expectExceptionMessage('Deposit amount must be greater than zero.');
+
+        $subject->deposit(0.00);
     }
 }
