a63fa844cb67096dd8f8df8432baa7399f40720c
Michael Phillips <michaeljoelphillips@gmail.com>
2019-10-11 00:23:36 -0500

    Add findByDateRange to the ledger.

--- tests/Unit/LedgerTest.php
+++ tests/Unit/LedgerTest.php
@@ -6,6 +6,9 @@ namespace Tests\Unit;
 
 use App\Deposit;
 use App\Ledger;
+use App\TransactionInterface;
+use DateInterval;
+use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
 
 class LedgerTest extends TestCase
@@ -25,5 +28,32 @@ class LedgerTest extends TestCase
         $subject = new Ledger();
 
         $this->assertEquals(0.0, $subject->getBalance());
+    }
+
+    public function testFindByDateRange(): void
+    {
+        $subject = new Ledger();
+
+        $thisMonthsTransaction = $this->createMock(TransactionInterface::class);
+        $lastMonthsTransaction = $this->createMock(TransactionInterface::class);
+
+        $subject->addTransaction($thisMonthsTransaction);
+        $subject->addTransaction($lastMonthsTransaction);
+
+        $thisMonthsTransaction
+            ->method('getTransactionDate')
+            ->willReturn(new DateTimeImmutable('2019-10-01'));
+
+        $lastMonthsTransaction
+            ->method('getTransactionDate')
+            ->willReturn(new DateTimeImmutable('2019-09-01'));
+
+        $thisMonthsTransactions = $subject->findByDateRange(new DateTimeImmutable('2019-10-01'), new DateInterval('P1M'));
+        $lastMonthsTransactions = $subject->findByDateRange(new DateTimeImmutable('2019-09-01'), new DateInterval('P1M'));
+
+        self::assertCount(1, $thisMonthsTransactions);
+        self::assertCount(1, $lastMonthsTransactions);
+        self::assertContains($thisMonthsTransaction, $thisMonthsTransactions);
+        self::assertContains($lastMonthsTransaction, $lastMonthsTransactions);
     }
 }
